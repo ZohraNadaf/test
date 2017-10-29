f15b56d67dce371dd7652220725e80981fedc28b
Zohra Nadaf <zohra1462n@gmail.com>
2017-10-29 13:42:57 -0500

    first commit

---
+++ objectCreation.js
@@ -0,0 +1,15 @@
+let dog = {
+	sound : 'woof',
+	talk: function(){
+		console.log(this.sound)
+	}
+}
+
+dog.talk() // "woof"
+
+let talkFunction  = dog.talk
+talkFunction() //undefined
+
+let boundFuntion = 
+    talkFunction.bind(dog)
+boundFuntion() //"woof"
